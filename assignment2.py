7c1ee500a57602095ac3fcd05b4d166deb80f21a
ravinderatri <40792897+ravinderatri@users.noreply.github.com>
2022-02-21 19:50:21 +0530

    find the index of all "we"

---
+++ assignment2.py
@@ -0,0 +1,11 @@
+str3="what we think we become"
+print(str3[6])
+str4="welcome to python learning" # start find from (me) passed character
+print(str4[str4.find("me"):])
+str4="what we think we become we"
+index=0
+for each in str4: 
+    if str4.find("we"):
+     print(each)
+        
+print(each)
