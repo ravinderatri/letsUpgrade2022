f6532e3921a63c62bb2de082adbd20f4a3fb9d08
ravinderatri <40792897+ravinderatri@users.noreply.github.com>
2022-02-21 19:51:14 +0530

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,7 +1,4 @@
-str3="what we think we become"
-print(str3[6])
-str4="welcome to python learning" # start find from (me) passed character
-print(str4[str4.find("me"):])
+ 
 str4="what we think we become we"
 index=0
 for each in str4: 
