3d70000757e5c269e395b671559ba858e18346a1
ravinderatri <40792897+ravinderatri@users.noreply.github.com>
2022-03-09 19:57:15 +0530

    find the duplicate line in sample

---
+++ assignment08-3-22.py
@@ -0,0 +1,14 @@
+
+fr=open("sample.txt","r")
+#d=fr.readlines()
+list1=[] 
+for line in fr:
+    list1 = line 
+    if list1 in list1:        
+        print(list1)
+    else:
+        print(list1)
+   
+     
+ 
+fr.close()
